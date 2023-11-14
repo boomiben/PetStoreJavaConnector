e76ddd467e38b6662e231fd746756ef04d2d8657
Ben DeBoer <ben.deboer@boomi.com>
2023-11-14 15:09:27 -0800

    Quick fixes to enable compile

--- src/main/java/com/boomi/connector/PetStore/PetStoreConnector.java
+++ src/main/java/com/boomi/connector/PetStore/PetStoreConnector.java
@@ -1,5 +1,6 @@
 package com.boomi.connector.PetStore;
 
+import com.boomi.connector.PetStore.operations.CreateOperation;
 import com.boomi.connector.PetStore.operations.GetOperation;
 import com.boomi.connector.api.BrowseContext;
 import com.boomi.connector.api.Browser;
@@ -35,7 +36,7 @@ public class PetStoreConnector extends BaseConnector
     @Override
     protected Operation createCreateOperation(OperationContext context)
     {
-        return new PetStoreCreateOperation(createConnection(context));
+        return new CreateOperation(createConnection(context));
     }
  
 }
